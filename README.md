f60f043f569574001c63ccb0786fe4b74adb7720
Alexandre Russel <alexandre@russel.fr>
2021-03-26 09:43:10 +0100

    Hanging on aws_security_group_rule

--- README.md
+++ README.md
@@ -5,17 +5,17 @@
    2. `terraform apply`
 
 # Issue with local stack:
-If all the lines of provider "aws" are commented out, and with proper aws credentials set, then the code run and create
-the expected subnet in AWS. But against localstack, it output an error:
+Runnning this in AWS returns promply the creation of the 3 resources. But against localstack, it hangs. We can see in
+the logs that the rule were created correctly but terraform seems unable to understand it:
 
 ```
-aws_vpc.redshift_vpc: Creating...
-aws_vpc.redshift_vpc: Creation complete after 1s [id=vpc-4468e533]
-aws_subnet.redshif_subnet: Creating...
-aws_subnet.redshif_subnet: Creation complete after 1s [id=subnet-e7cb48ee]
-aws_redshift_subnet_group.redshift_subnet_group: Creating...
-
-Error: Unable to find Redshift Subnet Group: []*redshift.ClusterSubnetGroup(nil)
+2021-03-26T09:38:12.074+0100 [DEBUG] plugin.terraform-provider-aws_v3.22.0_x5: 2021/03/26 09:38:12 [DEBUG] [aws-sdk-go] <DescribeSecurityGroupsResponse xmlns="http://ec2.amazonaws.com/doc/2013-10-15/"><requestId>59dbff89-35bd-4eac-99ed-be587EXAMPLE</requestId><securityGroupInfo><item><ownerId>000000000000</ownerId><groupId>sg-1cf94f40</groupId><groupName>terraform-20210326083754707700000001</groupName><groupDescription>Managed by Terraform</groupDescription><vpcId>vpc-dd000f17</vpcId><ipPermissions><item><ipProtocol>tcp</ipProtocol><fromPort>5439</fromPort><toPort>5439</toPort><groups></groups><ipRanges><item><cidrIp>0.0.0.0/0</cidrIp></item></ipRanges></item></ipPermissions><ipPermissionsEgress></ipPermissionsEgress><tagSet></tagSet></item></securityGroupInfo></DescribeSecurityGroupsResponse>
+2021-03-26T09:38:12.074+0100 [DEBUG] plugin.terraform-provider-aws_v3.22.0_x5: 2021/03/26 09:38:12 [DEBUG] Unable to find matching ingress Security Group Rule (sgrule-1692235148) for Group sg-1cf94f40
+2021-03-26T09:38:12.074+0100 [DEBUG] plugin.terraform-provider-aws_v3.22.0_x5: 2021/03/26 09:38:12 [TRACE] Waiting 10s before next try
+2021/03/26 09:38:13 [TRACE] dag/walk: vertex "root" is waiting for "meta.count-boundary (EachMode fixup)"
+2021/03/26 09:38:13 [TRACE] dag/walk: vertex "meta.count-boundary (EachMode fixup)" is waiting for "aws_security_group_rule.allow_redshift_connection"
+2021/03/26 09:38:13 [TRACE] dag/walk: vertex "provider[\"registry.terraform.io/hashicorp/aws\"] (close)" is waiting for "aws_security_group_rule.allow_redshift_connection"
+aws_security_group_rule.allow_redshift_connection: Still creating... [20s elapsed]
 ```
 
 Removing the `aws_redshift_subnet_group` fixes the problem. Adding a `aws_redshift_cluster` that references the subnet 
--- main.tf
+++ main.tf
@@ -3,55 +3,41 @@ terraform {
 }
 
 variable "endpoint_port" {
-  type = string
+  type    = string
   default = "4566"
 }
 
 provider "aws" {
-  access_key = "access"
-  secret_key = "secret"
-  region = "us-east-1"
-  s3_force_path_style = true
+  access_key                  = "access"
+  secret_key                  = "secret"
+  region                      = "us-east-1"
+  s3_force_path_style         = true
   skip_credentials_validation = true
-  skip_metadata_api_check = true
-  skip_requesting_account_id = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
   endpoints {
-    apigateway = "http://localhost:${var.endpoint_port}"
-    cloudformation = "http://localhost:${var.endpoint_port}"
-    cloudwatch = "http://localhost:${var.endpoint_port}"
-    dynamodb = "http://localhost:${var.endpoint_port}"
-    es = "http://localhost:${var.endpoint_port}"
-    ec2 = "http://localhost:${var.endpoint_port}"
-    firehose = "http://localhost:${var.endpoint_port}"
-    iam = "http://localhost:${var.endpoint_port}"
-    kinesis = "http://localhost:${var.endpoint_port}"
-    lambda = "http://localhost:${var.endpoint_port}"
-    cloudwatchlogs = "http://localhost:${var.endpoint_port}"
-    route53 = "http://localhost:${var.endpoint_port}"
+    ec2      = "http://localhost:${var.endpoint_port}"
+    iam      = "http://localhost:${var.endpoint_port}"
     redshift = "http://localhost:${var.endpoint_port}"
-    s3 = "http://localhost:${var.endpoint_port}"
-    secretsmanager = "http://localhost:${var.endpoint_port}"
-    ses = "http://localhost:${var.endpoint_port}"
-    sns = "http://localhost:${var.endpoint_port}"
-    sqs = "http://localhost:${var.endpoint_port}"
-    ssm = "http://localhost:${var.endpoint_port}"
-    stepfunctions = "http://localhost:${var.endpoint_port}"
-    sts = "http://localhost:${var.endpoint_port}"
   }
 }
 
 resource "aws_vpc" "redshift_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 }
 
-resource "aws_subnet" "redshift_subnet" {
-  cidr_block = "10.0.0.0/24"
+resource "aws_security_group" "redshift_sg" {
   vpc_id = aws_vpc.redshift_vpc.id
 }
-
-resource "aws_redshift_subnet_group" "redshift_subnet_group" {
-  name = "my-redshift-subnet-group"
-  subnet_ids = [
-    aws_subnet.redshift_subnet.id]
+resource "aws_security_group_rule" "allow_redshift_connection" {
+  from_port         = 5439
+  protocol          = "TCP"
+  security_group_id = aws_security_group.redshift_sg.id
+  to_port           = 5439
+  cidr_blocks = [
+  "0.0.0.0/0"]
+  ipv6_cidr_blocks = [
+  "::/0"]
+  type = "ingress"
 }
